64c54145eb748cb45b331cd9d79c73d9709f2aed
anindyadeffanabila@gmail.com <66814960+anindyadeffa@users.noreply.github.com>
2022-04-19 23:21:57 +0700

    edited path route

--- task-weekly/src/App.vue
+++ task-weekly/src/App.vue
@@ -9,7 +9,10 @@
       <v-app-bar-nav-icon @click.stop="drawer = !drawer">
         <font-awesome-icon icon="fa-solid fa-bars" />
       </v-app-bar-nav-icon>
-      <v-toolbar-title href="/">Anindya Deffa Nabila - Vue Kelas A
+      <v-toolbar-title>
+        <a href="/" class="title-nav">
+          Anindya Deffa Nabila - Vue Kelas A
+        </a>
       </v-toolbar-title>
       <v-spacer></v-spacer>
     </v-app-bar>
@@ -28,30 +31,50 @@
           v-model="group"
           active-class="deep-grey--text text--accent-4"
         >
-          <v-list-item href="/">
-              <font-awesome-icon icon="fa-solid fa-house" />
-              <v-list-item-title class="title-sidebar">Home</v-list-item-title>
+          <v-list-item :to="{ path: '/' }">
+            <font-awesome-icon icon="fa-solid fa-house" />
+            <v-list-item-title class="title-sidebar">Home</v-list-item-title>
           </v-list-item>
           <v-list-item :to="{ path: '/section-2' }">
-              <font-awesome-icon icon="fa-solid fa-book" />
-              <v-list-item-title class="title-sidebar">Section 2</v-list-item-title>
+            <font-awesome-icon icon="fa-solid fa-book" />
+            <v-list-item-title class="title-sidebar">Section 2</v-list-item-title>
           </v-list-item>
-          <v-list-item :to="{ path: '/' }">
-              <font-awesome-icon icon="fa-solid fa-book" />
-              <v-list-item-title class="title-sidebar">Section 3</v-list-item-title>
+          <v-list-item :to="{ path: '/section-3' }">
+            <font-awesome-icon icon="fa-solid fa-book" />
+            <v-list-item-title class="title-sidebar">Section 3</v-list-item-title>
           </v-list-item>
           <v-list-item :to="{path: '/section-4'}">
-              <font-awesome-icon icon="fa-solid fa-book" />
-              <v-list-item-title class="title-sidebar">Section 4</v-list-item-title>
+            <font-awesome-icon icon="fa-solid fa-book" />
+            <v-list-item-title class="title-sidebar">Section 4</v-list-item-title>
           </v-list-item>
-          <v-list-item href="/section-5">
-              <font-awesome-icon icon="fa-solid fa-book" />
-              <v-list-item-title class="title-sidebar">Section 5</v-list-item-title>
+          <v-list-item :to="{path: '/section-5'}">
+            <font-awesome-icon icon="fa-solid fa-book" />
+            <v-list-item-title class="title-sidebar">Section 5</v-list-item-title>
           </v-list-item>
-          <v-list-item href="/section-6">
-              <font-awesome-icon icon="fa-solid fa-book" />
-              <v-list-item-title class="title-sidebar">Section 6</v-list-item-title>
+          <v-list-item :to="{path: '/section-6'}">
+            <font-awesome-icon icon="fa-solid fa-book" />
+            <v-list-item-title class="title-sidebar">Section 6</v-list-item-title>
           </v-list-item>
+          <!-- <v-list-item :to="{path: '/section-7'}">
+            <font-awesome-icon icon="fa-solid fa-book" />
+            <v-list-item-title class="title-sidebar">Section 7</v-list-item-title>
+          </v-list-item>
+          <v-list-item :to="{path: '/section-8'}">
+            <font-awesome-icon icon="fa-solid fa-book" />
+            <v-list-item-title class="title-sidebar">Section 8</v-list-item-title>
+          </v-list-item>
+          <v-list-item :to="{path: '/section-9'}">
+            <font-awesome-icon icon="fa-solid fa-book" />
+            <v-list-item-title class="title-sidebar">Section 9</v-list-item-title>
+          </v-list-item>
+          <v-list-item :to="{path: '/section-10'}">
+            <font-awesome-icon icon="fa-solid fa-book" />
+            <v-list-item-title class="title-sidebar">Section 10</v-list-item-title>
+          </v-list-item>
+          <v-list-item :to="{path: '/section-11'}">
+            <font-awesome-icon icon="fa-solid fa-book" />
+            <v-list-item-title class="title-sidebar">Section 11</v-list-item-title>
+          </v-list-item> -->
           <v-list-item class="github-icon">
             <v-btn icon
               href="https://github.com/anindyadeffa"
@@ -104,5 +127,9 @@ export default {
 }
 .github-icon {
   justify-content: center;
+}
+.title-nav {
+  text-decoration: none !important;
+  color: white !important;
 }
 </style>
